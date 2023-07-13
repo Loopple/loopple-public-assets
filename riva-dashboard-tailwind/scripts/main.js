d3043af6830e86466e1a9ceeb3d92f42268cbb30
Nepcha <27827318+rarestoma@users.noreply.github.com>
2023-07-13 18:17:41 +0300

    Feat(riva dashboard): Update css and js

--- riva-dashboard-tailwind/scripts/main.js
+++ riva-dashboard-tailwind/scripts/main.js
@@ -1347,3 +1347,57 @@ if (document.querySelector(".group\\/sidebar-menu")) {
     });
   });
 }
+
+
+if (document.querySelector("#map")) {
+
+  // Initialize the vector map
+  var map = new jsVectorMap({
+    selector: "#map",
+    map: "world_merc",
+    zoomOnScroll: false,
+    zoomButtons: true,
+    selectedMarkers: [1, 2],
+    markersSelectable: true,
+    onLoaded: function (event, tooltip, code) {
+      window.addEventListener("resize", function(){
+        this.map.updateSize();
+      });
+    },
+    markers: [{
+        name: "USA",
+        coords: [40.71296415909766, -74.00437720027804]
+      },
+      {
+        name: "Germany",
+        coords: [51.17661451970939, 10.97947735117339]
+      },
+      {
+        name: "Brazil",
+        coords: [-7.596735421549542, -54.781694323779185]
+      },
+      {
+        name: "Russia",
+        coords: [62.318222797104276, 89.81564777631716]
+      },
+      {
+        name: "China",
+        coords: [22.320178999475512, 114.17161225541399],
+      }
+    ],
+    markerStyle: {
+      initial: {
+        fill: "#0052ed"
+      },
+      hover: {
+        fill: "#09132d"
+      },
+      selected: {
+        fill: "#09132d"
+      }
+    },
+
+
+  });
+
+}
